f45d5e7c9dfe40a9c0f1524ee4d3a4fe6c61331d
Zack <zmoumen13@gmail.com>
2018-03-21 22:42:04 -0600

    added jsx

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./NavBar.css";
 
-const Score = props => {
-
-}
+const Score = props => (
+  <div>
+    <nav className="navbar navbar-default">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <a className="navbar-brand" href="/">
+            Clicky Game
+          </a>
+          <h2 className="score-heading">
+            Click an Image to Begin!
+          </h2>
+          <h2 className="score">
+            Score: {props.score}
+            |
+            Top Score: {props.topscore}
+          </h2>
+        </div>
+      </div>
+    </nav>
+  </div>
+)
 
 export default Score;
