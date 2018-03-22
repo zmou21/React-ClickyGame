fa79e7a5b93366ea943ce8f21b7027e8e3666897
Zack <zmoumen13@gmail.com>
2018-03-21 22:42:32 -0600

    initial commit

---
+++ src/components/ContentMiddle/MiddleContent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./MiddleContent.css";
+
+
+const MiddleContent = () => (
+  <div>
+    <h2>
+      Test
+    </h2>
+  </div>
+);
+
+
+export default MiddleContent;
