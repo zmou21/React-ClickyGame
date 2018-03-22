ee5067a65da8a00368dca9fef501eeccde619646
Zack <zmoumen13@gmail.com>
2018-03-21 23:22:12 -0600

    added score shell functionality

--- src/App.js
+++ src/App.js
@@ -10,20 +10,26 @@ import "./App.css";
 class App extends React.Component {
 
   state = {
+    score: 0,
+    topScore: 0,
     friends
   }
 
-  removeFriend = id => {
+  handleClick = id => {
     const friends = this.state.friends.filter(friend => friend.id !== id);
     this.setState({friends})
   }
 
+
   render (){
     return (
       <div>
-        <NavBar />
-        <MiddleContent />
+        <NavBar
+          score= {this.state.score}
+          topScore = {this.state.topScore}
+          />
         <Wrapper>
+          <MiddleContent />
           {this.state.friends.map(friend => (
           <FriendCard
             removeFriend = {this.removeFriend}
