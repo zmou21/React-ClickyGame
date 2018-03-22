2fb5ffe880db0c963aecdd319a2b2eeb81ce49ad
Zack <zmoumen13@gmail.com>
2018-03-21 23:23:11 -0600

    updated with text

--- src/components/ContentMiddle/MiddleContent.js
+++ src/components/ContentMiddle/MiddleContent.js
@@ -3,10 +3,14 @@ import "./MiddleContent.css";
 
 
 const MiddleContent = () => (
-  <div>
-    <h2>
-      Test
+
+  <div className="middle">
+    <h2 className="middle-text">
+      Lets Play!
     </h2>
+    <h3 className="middle-para">
+      Click on an image to earn points, but don't click on any more than once!
+    </h3>
   </div>
 );
 
