4b6081f3dba399a66d96a3eb1086f18ebdf4ccf2
Zack <zmoumen13@gmail.com>
2018-03-21 23:24:34 -0600

    updated with list navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,23 +3,13 @@ import "./NavBar.css";
 
 const Score = props => (
   <div>
-    <nav className="navbar navbar-default">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <a className="navbar-brand" href="/">
-            Clicky Game
-          </a>
-          <h2 className="score-heading">
-            Click an Image to Begin!
-          </h2>
-          <h2 className="score">
-            Score: {props.score}
-            |
-            Top Score: {props.topscore}
-          </h2>
-        </div>
-      </div>
-    </nav>
+  <nav className="navbar">
+    <ul>
+      <li className="brand"><a href="/">Clicky Game</a></li>
+      <li>Click an image to begin!</li>
+      <li>Score: {props.score} | Top Score: {props.topScore}</li>
+    </ul>
+  </nav>
   </div>
 )
 
