c2e3bf18b5b224254ec6d3f719f6fd789f19bdc2
Zack <zmoumen13@gmail.com>
2018-03-21 22:41:13 -0600

    add navbar component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import friends from "./friends.json";
+import NavBar from "./components/NavBar";
+import MiddleContent from "./components/ContentMiddle"
 import "./App.css";
 
 
@@ -18,20 +20,23 @@ class App extends React.Component {
 
   render (){
     return (
-      <Wrapper>
-        <h1 className="title">Friends List</h1>
-        {this.state.friends.map(friend => (
-        <FriendCard
-          removeFriend = {this.removeFriend}
-          id={friend.id}
-          key={friend.id}
-          name={friend.name}
-          image={friend.image}
-          occupation={friend.occupation}
-          location={friend.location}
-        />
-        ))}
-      </Wrapper>
+      <div>
+        <NavBar />
+        <MiddleContent />
+        <Wrapper>
+          {this.state.friends.map(friend => (
+          <FriendCard
+            removeFriend = {this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+          ))}
+        </Wrapper>
+      </div>
     );
   }
 };
